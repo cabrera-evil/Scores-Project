8a40b42f4b5bbd0ad1296b0add4470d2211d633a
cabrera-evil <douglascabrera.dev@gmail.com>
2022-12-15 17:03:43 -0600

    Update: Sidebar

--- Client/assets/js/index.js
+++ Client/assets/js/index.js
@@ -79,9 +79,11 @@ document.addEventListener('DOMContentLoaded', () => {
     // Display admin panel if user is admin
     if (role == 'ADMIN_ROLE') {
       admin.style.display = 'block';
+      document.getElementById('addons').style.display = 'block';
     }
     else {
       admin.style.display = 'none';
+      document.getElementById('addons').style.display = 'none';
     }
 
     // Redirect to dashboard if we have a token (it's already logged)
