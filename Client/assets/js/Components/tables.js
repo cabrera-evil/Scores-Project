0c2bbc3bd96ec96618ad3a2041af2e6c5e1f2f3b
cabrera-evil <douglascabrera.dev@gmail.com>
2022-12-15 12:57:13 -0600

    Update: Get subject data

--- Client/assets/js/Components/tables.js
+++ Client/assets/js/Components/tables.js
@@ -15,37 +15,52 @@ function parseJwt(token) {
 if (window.location.pathname === '/subjects.html') {
     const user = parseJwt(localStorage.getItem('token')).uid;
     const career_id = JSON.parse(localStorage.getItem('user')).career_id;
+    // Document elements
+    const uv = document.getElementById('uv');
+    const year = document.getElementById('year');
 
-    // Axios get subjects by career_id
-    axios.get(user_url + "?_id=" + user)
-        .then(response => {
-            const subjects = response.data.users[0].subjects;
-            const select = document.getElementById('subjects');
-            subjects.forEach(subject => {
+    year.addEventListener('change', async () => {
+        // Delete options
+        const options = document.querySelectorAll('#subjects option');
+        options.forEach(option => {
+            if (option.value) {
+                option.remove();
+            }
+        });
 
+        // Axios get subjects by career_id
+        axios.get(faculty_url)
+            .then(response => {
+                const subjects = response.data.faculties[0].careers;
+                const select = document.getElementById('subjects');
+
+                subjects.forEach(career => {
+                    if (career.id === career_id) {
+                        career.subjects.forEach(subject => {
+                            if (subject.year == year.value) {
+                                const option = document.createElement('option');
+                                option.value = subject.id;
+                                option.innerText = subject.name;
+                                select.appendChild(option);
+
+                                select.addEventListener('change', () => {
+                                    // Get the selected subject value
+                                    const selected = select.options[select.selectedIndex].value;
+
+                                    // Get the selected subject uv
+                                    career.subjects.forEach(subject => {
+                                        if (subject.id == selected) {
+                                            uv.value = subject.uv;
+                                        }
+                                    }
+                                    );
+                                });
+                            }
+                        });
+                    }
+                });
             });
-        }
-        );
-
-    // Axios get subjects by career_id
-    axios.get(faculty_url)
-
-        .then(response => {
-            const subjects = response.data.faculties[0].careers;
-            const select = document.getElementById('subjects');
-
-            subjects.forEach(subject => {
-                if (subject.id === career_id) {
-                    subject.subjects.forEach(subject => {
-                        const option = document.createElement('option');
-                        option.value = subject.id;
-                        option.innerText = subject.name;
-                        select.appendChild(option);
-                    });
-                }
-            });
-        }
-        );
+    });
 
     // On submit
     const frmSubjects = document.getElementById('frm-subjects');
@@ -63,12 +78,11 @@ if (window.location.pathname === '/subjects.html') {
     );
 
     // Register subjects request
-    function registerSubjects(data) {
+    async function registerSubjects(data) {
         axios.patch(`${user_url}/${user}`, {
             subjects: {
                 id: data.subjects,
-                name: 'PRE CALCULUS',
-                time: data.time
+                times: data.time
             }
         },
             {
