07b3ce992b632ee8fc5e70aad4052b95f7f16fbe
cabrera-evil <douglascabrera.dev@gmail.com>
2022-12-27 21:54:00 -0600

    Update: Package and fix mongoose warning

--- Server/config/database.js
+++ Server/config/database.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 // Connecting to Mongo Atlas database
 const connect = async () => {
     mongoose
+        .set('strictQuery', false)
         .connect(process.env.MONGODB_CNN)
         .then(() => { 
             console.log("Connected to MongoDB Atlas") 
